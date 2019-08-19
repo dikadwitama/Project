95fce1231065851bc4f25202b9a866c3e21e4046
dika <dikadwitama>
2019-08-19 19:24:48 +0700

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,25 @@
 // This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  appID: '671ac7a5d543c9935d09d1a2e6ebfb5b',
+  gKey: 'AIzaSyB674DWB6yR68yAr1RD8U4tAyCQj5K-ZG0',
+  config: {
+    apiKey: 'AIzaSyAoCbHJzrl2F5o0Xf1wvwH6SEAskVOZsMg',
+    authDomain: 'minimus-weather.firebaseapp.com',
+    databaseURL: 'https://minimus-weather.firebaseio.com',
+    projectId: 'minimus-weather',
+    storageBucket: 'minimus-weather.appspot.com',
+    messagingSenderId: '830228583555'
+  }
 };
 
 /*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
